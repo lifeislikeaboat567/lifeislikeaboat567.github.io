3c440492cce5abea768c333f8ff6cded1c789567
lifeislikeaboat567 <2648226350@qq.com>
2025-09-04 09:44:03 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	index.html

--- assets/NavbarsView-CAwG2Hbb.js
+++
@@ -1,8 +0,0 @@
-import{s as c,_ as i,a as p}from"./nav-pills-F7PRF06e.js";import{_ as m}from"./NavbarDefault-C6US93t1.js";import{d as r,j as t,o as _,g as a,f as e,v as l}from"./index-8q68W9hD.js";import"./_commonjsHelpers-D6-XlEtG.js";const u={name:"NavbarDark",setup(s){return(o,n)=>(t(),r(m,{dark:"",transparent:""}))}},f=`<script setup>
-// example component
-import NavbarDefault from "@/examples/navbars/NavbarDefault.vue";
-<\/script>
-<template>
-  <NavbarDefault dark transparent />
-</template>
-`,k={name:"NavbarsView",setup(s){return _(()=>{c()}),(o,n)=>(t(),r(i,{title:"Navbars",breadcrumb:[{label:"Navigation",route:"#"},{label:"Navbars"}]},{default:a(()=>[e(p,{title:"Navbar dark",code:l(f),id:"navbar-dark",height:"300  position-relative"},{default:a(()=>[e(u)]),_:1},8,["code"])]),_:1}))}};export{k as default};
