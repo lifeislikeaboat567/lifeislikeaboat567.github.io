356c8a9fe7343e24206b17ea12046c269556105a
lifeislikeaboat567 <2648226350@qq.com>
2025-09-04 16:04:17 +0800

    Add files via upload

---
+++ assets/FormsView-CKkf0pRS.js
@@ -0,0 +1,77 @@
+import{s as m,_ as n,a as p}from"./nav-pills-Nv_H9Yof.js";import{_ as o}from"./MaterialInput-C-gd3fM_.js";import{_ as u}from"./MaterialTextArea-BgUteeBa.js";import{_}from"./MaterialButton-P9yVWxBy.js";import{_ as f}from"./MaterialSwitch-CCSt54HY.js";import{o as i,z as h,c as v,j as l,b as t,f as e,g as s,k as a,d as b,v as x}from"./index-xTUJoJ3k.js";import"./NavbarDefault-DALxNKP2.js";import"./_commonjsHelpers-D6-XlEtG.js";const g={class:"container py-7"},M={class:"row"},y={class:"col-lg-7 mx-auto d-flex justify-content-center flex-column"},w=t("h3",{class:"text-center"},"Contact us",-1),k={role:"form",id:"contact-form",method:"post",autocomplete:"off"},C={class:"card-body"},S={class:"row"},I={class:"col-md-6"},B={class:"col-md-6 ps-2"},N={class:"mb-4"},A=a("Your message"),F={class:"row"},T={class:"col-md-12"},$=a(" I agree to the "),j=t("a",{href:"javascript:;",class:"text-dark"},[t("u",null,"Terms and Conditions")],-1),V=a(". "),E={class:"col-md-12"},D=a("Send Message"),L={name:"FormSimple",setup(c){return i(()=>{h()}),(r,d)=>(l(),v("section",null,[t("div",g,[t("div",M,[t("div",y,[w,t("form",k,[t("div",C,[t("div",S,[t("div",I,[e(o,{class:"input-group-dynamic mb-4",label:{text:"First Name",class:"form-label"},type:"text"})]),t("div",B,[e(o,{class:"input-group-dynamic",label:{text:"Last Name",class:"form-label"},type:"text"})])]),t("div",N,[e(o,{class:"input-group-dynamic",label:{text:"Email Address",class:"form-label"},type:"email"})]),e(u,{class:"input-group-static mb-4",id:"message",rows:4},{default:s(()=>[A]),_:1})]),t("div",F,[t("div",T,[e(f,{class:"mb-4 d-flex align-items-center",id:"flexSwitchCheckDefault",checked:"",labelClass:"ms-3 mb-0"},{default:s(()=>[$,j,V]),_:1}),t("div",E,[e(_,{type:"submit",variant:"gradient",color:"dark",fullWidth:""},{default:s(()=>[D]),_:1})])])])])])])])]))}},W=`<script setup>
+//Vue Material Kit 2 components
+import MaterialInput from "@/components/MaterialInput.vue";
+import MaterialTextArea from "@/components/MaterialTextArea.vue";
+import MaterialButton from "@/components/MaterialButton.vue";
+import MaterialSwitch from "@/components/MaterialSwitch.vue";
+<\/script>
+<template>
+  <section>
+    <div class="container py-4">
+      <div class="row">
+        <div class="col-lg-7 mx-auto d-flex justify-content-center flex-column">
+          <h3 class="text-center">Contact us</h3>
+          <form role="form" id="contact-form" method="post" autocomplete="off">
+            <div class="card-body">
+              <div class="row">
+                <div class="col-md-6">
+                  <MaterialInput
+                    class="input-group-dynamic mb-4"
+                    :label="{ text: 'First Name', class: 'form-label' }"
+                    type="text"
+                  />
+                </div>
+                <div class="col-md-6 ps-2">
+                  <MaterialInput
+                    class="input-group-dynamic"
+                    :label="{ text: 'Last Name', class: 'form-label' }"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div class="mb-4">
+                <MaterialInput
+                  class="input-group-dynamic"
+                  :label="{ text: 'Email Address', class: 'form-label' }"
+                  type="email"
+                />
+              </div>
+              <MaterialTextArea
+                class="input-group-static mb-4"
+                id="message"
+                :rows="4"
+                >Your message</MaterialTextArea
+              >
+            </div>
+            <div class="row">
+              <div class="col-md-12">
+                <MaterialSwitch
+                  class="mb-4 d-flex align-items-center"
+                  id="flexSwitchCheckDefault"
+                  checked
+                  labelClass="ms-3 mb-0"
+                >
+                  I agree to the
+                  <a href="javascript:;" class="text-dark"
+                    ><u>Terms and Conditions</u></a
+                  >.
+                </MaterialSwitch>
+
+                <div class="col-md-12">
+                  <MaterialButton
+                    type="submit"
+                    variant="gradient"
+                    color="dark"
+                    fullWidth
+                    >Send Message</MaterialButton
+                  >
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+`,O={name:"FormsView",setup(c){return i(()=>{m()}),(r,d)=>(l(),b(n,{title:"Forms",breadcrumb:[{label:"Input Areas",route:"#"},{label:"Forms"}]},{default:s(()=>[e(p,{title:"Form simple",code:x(W),id:"form-simple",height:"600"},{default:s(()=>[e(L)]),_:1},8,["code"])]),_:1}))}};export{O as default};
---
+++ assets/NavbarsView-DSyH11Jg.js
@@ -0,0 +1,8 @@
+import{s as c,_ as i,a as p}from"./nav-pills-Nv_H9Yof.js";import{_ as m}from"./NavbarDefault-DALxNKP2.js";import{d as r,j as t,o as _,g as a,f as e,v as l}from"./index-xTUJoJ3k.js";import"./_commonjsHelpers-D6-XlEtG.js";const u={name:"NavbarDark",setup(s){return(o,n)=>(t(),r(m,{dark:"",transparent:""}))}},f=`<script setup>
+// example component
+import NavbarDefault from "@/examples/navbars/NavbarDefault.vue";
+<\/script>
+<template>
+  <NavbarDefault dark transparent />
+</template>
+`,k={name:"NavbarsView",setup(s){return _(()=>{c()}),(o,n)=>(t(),r(i,{title:"Navbars",breadcrumb:[{label:"Navigation",route:"#"},{label:"Navbars"}]},{default:a(()=>[e(p,{title:"Navbar dark",code:l(f),id:"navbar-dark",height:"300  position-relative"},{default:a(()=>[e(u)]),_:1},8,["code"])]),_:1}))}};export{k as default};
